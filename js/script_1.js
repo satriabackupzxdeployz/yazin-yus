fad4a7623df5e5975be5c548d0d6176a87c6265c
satriabackupzxdeployz <satriabackupz@gmail.com>
2026-05-26 21:17:01 +0700

    Add js/script_1.js

---
+++ js/script_1.js
@@ -0,0 +1,11 @@
+
+  {
+    "@context": "https://schema.org",
+    "@type": "WebApplication",
+    "name": "ImgDrop",
+    "url": "https://imgdrop.web.id",
+    "description": "Image hosting service. Upload image to URL and get a direct link instantly.",
+    "applicationCategory": "UtilityApplication",
+    "operatingSystem": "Web"
+  }
+  
